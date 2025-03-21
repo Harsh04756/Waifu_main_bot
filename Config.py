23e8d0ddeb795a3cc0b513f363408dd754daefcb
Harsh04756 <alinwalker252@gmail.com>
2025-03-21 15:27:16 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+API_ID = 7583740884  # Your API ID
+API_HASH = "AAHNM9iVB-bWa3T4USseHrTavjBV8P6lAag"  # Your API Hash
+BOT_TOKEN = "8175826981:AAFXsU_YXbm3A2eDPrd4JALYMFt6fjKCK8Y"  # Your bot token
+MONGO_URL = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/?retryWrites=true&w=majority"
+BOT_USERNAME = "@Waifu_Chan_Robot"
+ADMIN_IDS = [1002372914024]  # Replace with your Telegram ID
+PHOTO_URL = "https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg"
